f23bf9793e3d18b29e66a635e65d6deaee0ae28e
SherstyukRoma <r.sherstuk@gmail.com>
2022-01-24 20:26:35 +0200

    finish

---
+++ SQLiteORM/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /*SQLiteConnector.CreateDatabaseSource("test.db");
+                SQLiteConnector.Connection.Open();
+                string createStudentTableQuery = "CREATE TABLE IF NOT EXISTS students (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio VARCHAR(128) NOT NULL, age INTEGER )";
+                    SQLiteCommand sQLiteCommand = new SQLiteCommand(createStudentTableQuery, SQLiteConnector.Connection);
+                sQLiteCommand.ExecuteNonQuery();
+                SQLiteConnector.Connection.Close();*/
+
+                string pathTofile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "database", "test.db");
+                SQLiteDBEngine dBEngine = new SQLiteDBEngine(pathTofile, SQLIteMode.EXISTS);
+
+
+                //Console.WriteLine(dBEngine["student"].Name);
+
+                SQLiteTable Students = dBEngine["student"];
+
+                foreach (SQLiteColumn col in Students.HeadRowInfo)
+                {
+                    Console.WriteLine(col);
+                }
+                Console.WriteLine("Данные таблицы");
+                foreach (var col in Students.BodyRows)
+                {
+                    Console.WriteLine($"ID: {col.Key}");
+                    foreach (var item in col.Value)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+
+                /*long lastIndex = Students.BodyRows.Count + 1;
+
+                List<string> newRow = new List<string>();
+                    newRow.Add("Вася");
+                    newRow.Add(15.ToString());
+                Students.BodyRows.Add(lastIndex, newRow);*/
+
+                //Students.AddOneRow(new List<string> { 45.ToString(), "Маклауд" });
+
+
+                dBEngine.Async();
+
+
+                List<KeyValuePair<string, string>> searchPattern = new List<KeyValuePair<string, string>>();
+                
+                searchPattern.Add(new KeyValuePair<string, string>("age", "68"));
+                searchPattern.Add(new KeyValuePair<string, string>("fio", "Semen Semenfovich"));
+                searchPattern.Add(new KeyValuePair<string, string>("Id", 6.ToString()));
+                
+
+                KeyValuePair<long, List<string>>?  findedStudent = Students.GetOneRow(searchPattern);
+
+                
+
+
+
+
+                Console.WriteLine("Данные таблицы");
+                foreach (var col in Students.BodyRows)
+                {
+                    Console.WriteLine($"ID: {col.Key}");
+                    foreach (var item in col.Value)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+                /*foreach (var item in dBEngine.Tables)
+                {
+                    Console.WriteLine(item);
+                }
+                dBEngine.getTableFields("student");*/
+
+                //string[] str = { };
+                /*SQLiteColumn sQLiteColumn = new SQLiteColumn(str);
+                //sQLiteColumn.GetDateType("VARCHAR(128)");
+                Console.WriteLine(sQLiteColumn.DataType);
+                */
+
+                /* SQLiteRow sQLiteRow = new SQLiteRow(3);
+                 foreach (var item in sQLiteRow)
+                 {
+                     Console.WriteLine(item);
+                 }*/
+                //Console.WriteLine(sQLiteRow[0]);
+                //Console.WriteLine(sQLiteRow["id"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SQLiteORM/SQLiteTable.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORM
+{
+    class SQLiteTable
+    {
+        private string _name;
+        private SQLiteRow _headRow;
+        private SortedList<long, List<string>> _bodyRows;
+        public SQLiteRow HeadRowInfo
+        {
+            get
+            {
+                return _headRow;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public SortedList<long, List<string>> BodyRows
+        {
+            get
+            {
+                return _bodyRows;
+            }
+        }
+        public SQLiteTable(string tableName, SQLiteRow headRow, SortedList<long, List<string>> bodyRows)
+        {
+            _name = tableName; //валидация?
+            _headRow = headRow;
+            _bodyRows = bodyRows;
+        }
+
+        public void AddOneRow(List<string> row)
+        {
+            this.BodyRows.Add(_bodyRows.Count + 1, row);
+
+            string query = $"INSERT INTO {_name} (";
+            foreach (SQLiteColumn column in _headRow)
+            {
+                query += column.Name + ", ";
+            }
+            query = query.Substring(0, query.Length - 2);
+            query += $") VALUES ( {_bodyRows.Count + 1}, ";
+
+            foreach (string item in row)
+            {
+                query += "'" + item + "', ";
+            }
+            query = query.Substring(0, query.Length - 2);
+            query += ")";
+
+            SQLiteDBEngine.AsyncQuery.Add(query);
+        }
+        public void DeleteOneRow(long Id)
+        {
+            if (BodyRows.ContainsKey(Id))
+            {
+                BodyRows.Remove(Id);
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect row Id");
+            }
+        }
+        public KeyValuePair<long, List<string>> GetOneRow(long Id)
+        {
+            if (BodyRows.ContainsKey(Id))
+            {
+                return new KeyValuePair<long, List<string>>(Id, BodyRows[Id]);
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect row Id");
+            }
+        }
+        public KeyValuePair<long, List<string>>? GetOneRow(List<KeyValuePair<string, string>> searchPattern)
+        {
+            bool searchMatched;
+            foreach (KeyValuePair<long, List<string>> oneRow in BodyRows)
+            {
+                searchMatched = true;
+                foreach (KeyValuePair<string, string> pattern in searchPattern)
+                {
+                    int indexCol = HeadRowInfo[pattern.Key].Cid;
+
+                    if (indexCol != 0)
+                    {
+                        if (oneRow.Value[indexCol - 1] != pattern.Value)
+                        {
+                            searchMatched = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (oneRow.Key != Int64.Parse(pattern.Value))
+                        {
+                            searchMatched = false;
+                            break;
+                        }
+                    }
+                }
+                if (searchMatched)
+                {
+                    return oneRow;
+                }
+            }
+            return null;
+        }
+        public bool UpdateOneRow(long Id, List<string> newData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
